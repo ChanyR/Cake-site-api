becc7dfe66caafbffb54db99080b5d06f198ca4a
ChanyR <c7195671@gmail.com>
2024-01-03 11:54:09 +0200

    chany reset passowrd

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,31 +1,42 @@
 const express = require("express");
-const { UserModel, validateUser, loginValid, createToken } = require("../models/userModel");
+const {
+  UserModel,
+  validateUser,
+  loginValid,
+  createToken,
+} = require("../models/userModel");
 const { auth, authAdmin } = require("../middlewares/auth");
-const userControllers=require("../controllers/users")
+const userControllers = require("../controllers/users");
 const router = express.Router();
 
 // domain/users
-router.get("/", userControllers.get)
+router.get("/", userControllers.get);
 
 // domain/users/myInfo
-router.get("/myInfo", auth, userControllers.getUserInfo)
+router.get("/myInfo", auth, userControllers.getUserInfo);
 
 // domain/users/usersList
-router.get("/usersList", authAdmin, userControllers.getUserList)
+router.get("/usersList", authAdmin, userControllers.getUserList);
 
 // domain/users
-router.post("/", userControllers.addUser)
+router.post("/", userControllers.addUser);
 
 // domain/users/login
-router.post("/login", userControllers.login)
+router.post("/login", userControllers.login);
 
 // domain/users/:editId
 router.put("/:editId", auth, userControllers.editUserById);
 
 // domain/users/changeToBaker/:userId
-router.put("/changeToBaker/:userId", authAdmin, userControllers.changeToBaker)
+router.put("/changeToBaker/:userId", authAdmin, userControllers.changeToBaker);
 
 // domain/users/:delId
 router.delete("/:delId", auth, userControllers.deleteUserById);
+
+// domain/users/sendEmail
+router.post("/sendEmail", userControllers.sendEmail);
+
+// domain/users/resetPassword
+router.patch("/resetPassword", userControllers.resetPassword);
 
 module.exports = router;
